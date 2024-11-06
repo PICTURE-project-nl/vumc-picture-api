eda9b909300ff5b0e5c53a6e532922f791ce4153
Raymond Martens <raymond.martens@activecollective.nl>
2024-11-06 20:56:43 +0100

    Refactor entrypoint.sh to improve execution order and ensure database migrations and user creation run smoothly

--- laravel/entrypoint.sh
+++ laravel/entrypoint.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
+# Environment variables
 DB_HOST=${DB_HOST}
 DB_CONNECTION=${DB_CONNECTION}
 DB_DATABASE=${MYSQL_DATABASE}
 DB_USERNAME=${MYSQL_USER}
 DB_PASSWORD=${MYSQL_PASSWORD}
 
-# Apache configuratie
+echo "Entrypoint.sh credentials check:"
+echo "DB_HOST=${DB_HOST}"
+echo "DB_CONNECTION=${DB_CONNECTION}"
+echo "DB_DATABASE=${DB_DATABASE}"
+echo "DB_USERNAME=${DB_USERNAME}"
+
+# Configure Apache
 sed -i "s/LARAVEL_PROJECT_NAME/${LARAVEL_PROJECT_NAME}/g" /etc/apache2/sites-available/000-default.conf
 sed -i "s/SERVER_NAME/${SERVER_NAME}/g" /etc/apache2/sites-available/000-default.conf
 apache2ctl graceful
 
-# Verplaats naar Laravel-directory
+# Move to Laravel directory
 cd /var/www/laravel
 
 # Laravel Project Setup
@@ -19,7 +26,7 @@ if [ ! -d "${LARAVEL_PROJECT_NAME}" ]; then
   composer create-project --prefer-dist laravel/laravel ${LARAVEL_PROJECT_NAME}
   cd ${LARAVEL_PROJECT_NAME}
 
-  # Laravel Passport en Keycloak configuratie
+  # Laravel Passport and Keycloak configuration
   composer require laravel/passport
   php artisan vendor:publish  --provider="KeycloakGuard\KeycloakGuardServiceProvider"
 
@@ -38,7 +45,7 @@ if [ ! -d "${LARAVEL_PROJECT_NAME}" ]; then
   php artisan storage:link
 fi
 
-# Specifieke omgevingsinstellingen en Swagger configuratie per omgeving
+# Check environment settings and apply them
 if [ -f "${LARAVEL_PROJECT_NAME}/.env" ]; then
   cd ${LARAVEL_PROJECT_NAME}
 
@@ -46,11 +53,9 @@ if [ -f "${LARAVEL_PROJECT_NAME}/.env" ]; then
     sed -i "/APP_ENV/c\APP_ENV=local" .env
     sed -i "/APP_DEBUG/c\APP_DEBUG=true" .env
     sed -i "/APP_LOG_LEVEL/c\APP_LOG_LEVEL=debug" .env
-
   elif [ "${APP_ENV}" == 'dev' ]; then
     sed -i "/APP_ENV/c\APP_ENV=staging" .env
     sed -i "/APP_DEBUG/c\APP_DEBUG=true" .env
-
   elif [ "${APP_ENV}" == 'prod' ]; then
     sed -i "/APP_ENV/c\APP_ENV=production" .env
     sed -i "/APP_DEBUG/c\APP_DEBUG=false" .env
@@ -58,18 +63,18 @@ if [ -f "${LARAVEL_PROJECT_NAME}/.env" ]; then
   fi
 fi
 
-# Beheer rechten en directory-structuur voor opslag
+# Manage permissions for storage directory
 mkdir -p ./storage/framework/{sessions,views,cache} ./storage/app/{public/{nifti,l,h},dicom-unprocessed,logs}
 chmod -R 777 storage
 chown -R www-data:www-data storage
 
-# Voer migraties uit en installeer Passport
+# Run migrations and Passport installation
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate --force || { echo "Migration failed"; exit 1; }
 echo "Passport installation..."
-php artisan passport:install
+php artisan passport:install || { echo "Passport installation failed"; exit 1; }
 
-# Start Apache en Supervisor
+# Start Apache and Supervisor
 apache2ctl -D FOREGROUND
 /usr/bin/supervisord -c /etc/supervisord.conf
 supervisorctl -c /etc/supervisord.conf start laravel-worker:*
